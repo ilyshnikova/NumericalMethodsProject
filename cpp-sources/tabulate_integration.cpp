f5249692a02038b956c26b97a1958c82fca6b1d2
Anastasiya Tsarkova <ilyshnikova@yandex.ru>
2016-11-05 14:25:05 +0300

    Adding integrate module

--- cpp-sources/tabulate_integration.cpp
+++ cpp-sources/tabulate_integration.cpp
@@ -1,5 +1,44 @@
+#include "tabular_function.h"
 #include "tabulate_integration.h"
 
 TabularFunction TabulateIntegration::Integrate(const TabularFunction& function) const {
-	return function;
+	TabularFunction integrated;
+	TabularFunction::iterator it = function.Begin();
+
+	double integral_value = 0;
+	bool first_point = true;
+	double prev_y = 0;
+	double prev_x = 0;
+	while (it.IsDefine()) {
+		double x = it.GetX();
+		double y = it.GetY();
+
+		if (!first_point) {
+			integral_value += (y + prev_y) / 2 * (x - prev_x);
+		}
+
+		integrated.AddValue(x, integral_value);
+		it.Next();
+		first_point = false;
+		prev_y = y;
+		prev_x = x;
+	}
+
+	return integrated;
 }
+
+extern "C" {
+	TabulateIntegration* TabulateIntegration_New() {
+		return new TabulateIntegration();
+	}
+
+
+	TabularFunction* TabulateIntegration_Integrate(const TabulateIntegration& self, const TabularFunction& function) {
+	       return new TabularFunction(self.Integrate(function));
+      	}
+
+	void TabulateIntegration_Delete(const TabulateIntegration& self) {
+		delete &self;
+	}
+}
+
---
+++ flask-server/modules/integration.py
@@ -0,0 +1,8 @@
+from dll_importer import BaseImport
+
+class TabulateIntegration(BaseImport):
+    SO_NAME = '../cpp-sources/numerical_methods.so'
+    RETURN_TYPES = {}
+
+
+
