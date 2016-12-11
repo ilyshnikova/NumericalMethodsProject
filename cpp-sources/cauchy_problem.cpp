0f86b7af3aaea7e24ba54f250277232c00d76b54
Anastasiya Tsarkova <nastyats@olegts.haze.yandex.net>
2016-12-11 19:31:59 +0300

    Generalizing cauchy task, improving module testing.

--- cpp-sources/cauchy_problem.cpp
+++ cpp-sources/cauchy_problem.cpp
@@ -1,27 +1,62 @@
 #include "cauchy_problem.h"
 
+TabularFunction CauchySolution::GetX() const {
+	return x;
+}
+
+TabularFunction CauchySolution::GetY() const {
+	return y;
+}
+
 CauchySolution::CauchySolution(const TabularFunction& x, const TabularFunction& y)
 	: x(x)
 	, y(y)
 {}
 
+CauchySolution CauchyProblem::Solve() const{
+	TabularFunction x, y;
+
+	double prev_x = x0;
+	double prev_y = y0;
+	double prev_t = t_min;
+
+	double t_cur = t_min;
+	while (t_cur <= t_max) {
+		if (prev_x > x_max || prev_x < x_min || prev_y > y_max || prev_y < y_min) {
+			break;
+		} else {
+			double x_cur = prev_x + (t_cur - prev_t) * f->GetValueX(prev_x, prev_y, prev_t);
+			double y_cur = prev_y + (t_cur - prev_t) * f->GetValueY(prev_x, prev_y, prev_t);
+
+			x.AddValue(t_cur, x_cur);
+			y.AddValue(t_cur, y_cur);
+
+			prev_x = x_cur;
+			prev_y = y_cur;
+			prev_t = t_cur;
+
+			t_cur += t_step;
+		}
+	}
+
+	return CauchySolution(x, y);
+}
+
+
 extern "C" {
 	CauchyProblem* CauchyProblem_New(
 		const double x0,
 		const double y0,
-		const double betta,
-		const double T,
-		const PolynomialFunction& U,
-		const PolynomialFunction& S,
-		const PolynomialFunction& z,
-		const F1& f
-
+		const double x_min,
+		const double x_max,
+		const double y_min,
+		const double y_max,
+		const double t_min,
+		const double t_max,
+		const double t_step,
+		BaseCauchyFunction* f
 	) {
-		return new CauchyProblem(x0, y0, betta, T, U, S, z, f);
-	}
-
-	CauchySolution* CauchySolution_New() {
-		return new CauchySolution();
+		return new CauchyProblem(x0, y0, x_min, x_max, y_min, y_max, t_min, t_max, t_step, f);
 	}
 
 	CauchySolution* CauchySolution_Copy(const CauchySolution& cauchy_solution) {
--- cpp-sources/functions.cpp
+++ cpp-sources/functions.cpp
@@ -1,24 +1,98 @@
+#include <iostream>
+#include <map>
+#include <cmath>
 #include "functions.h"
 
+template
+<typename T>
+const T * find_closest(const std::map<double, T> * some_map, const double some_key) {
+	double lower_key = some_map->lower_bound(some_key)->first;
+	double upper_key = some_map->upper_bound(some_key)->first;
+
+	if (fabs(lower_key - some_key) < fabs(upper_key - some_key)) {
+		return &(some_map->at(lower_key));
+	} else {
+		return &(some_map->at(upper_key));
+	}
+}
+
+void TabularCauchyFunction::AddValueX(const double x, const double y, const double t, const double value) {
+	if (x_values.count(x) == 0) {
+	       x_values[x] = std::map<double, std::map<double, double> >();
+      	}
+	if (x_values[x].count(y) == 0) {
+		x_values[x][y] = std::map<double, double>();
+	}
+	x_values[x][y][t] = value;
+}
+
+void TabularCauchyFunction::AddValueY(const double x, const double y, const double t, const double value) {
+	if (y_values.count(x) == 0) {
+	       y_values[x] = std::map<double, std::map<double, double> >();
+      	}
+	if (y_values[x].count(y) == 0) {
+		x_values[x][y] = std::map<double, double>();
+	}
+	y_values[x][y][t] = value;
+}
+
+double TabularCauchyFunction::GetValueX(const double x, const double y, const double t) const {
+	return *(find_closest(find_closest(find_closest(&x_values, x), y), t));
+}
+
+double TabularCauchyFunction::GetValueY(const double x, const double y, const double t) const {
+	return *(find_closest(find_closest(find_closest(&y_values, x), y), t));
+}
+
+double CircleCauchyFunction::GetValueX(const double x, const double y, const double t) const {
+	return y;
+}
+
+double CircleCauchyFunction::GetValueY(const double x, const double y, const double t) const {
+	return -x;
+}
+
+double SpiralCauchyFunction::GetValueX(const double x, const double y, const double t) const {
+	return x + y;
+}
+
+double SpiralCauchyFunction::GetValueY(const double x, const double y, const double t) const {
+	return y -  20 * x;
+}
+
+
+
 extern "C" {
-	F1* F1_New() {
-		return new F1();
+	TabularCauchyFunction* TabularCauchyFunction_New() {
+		return new TabularCauchyFunction();
 	}
 
-    void F1_SetBetta(F1& self, double betta) {
-        self.SetBetta(betta);
-    }
+	void TabularCauchyFunction_AddValueX(TabularCauchyFunction& self, const double x, const double y, const double t, const double value) {
+		self.AddValueX(x, y, t, value);
+	}
 
-    void F1_SetS(F1& self, const PolynomialFunction& s) {
-        self.SetS(s);
-    }
+	void TabularCauchyFunction_AddValueY(TabularCauchyFunction& self, const double x, const double y, const double t, const double value) {
+		self.AddValueY(x, y, t, value);
+	}
 
-    void F1_SetZ(F1& self, const PolynomialFunction& z) {
-        self.SetZ(z);
-    }
+	void TabularCauchyFunction_Delete(const TabularCauchyFunction& self) {
+		delete &self;
+	}
 
-    void F1_Delete(const F1& func) {
-        	delete &func;
+	CircleCauchyFunction* CircleCauchyFunction_New() {
+		return new CircleCauchyFunction();
+	}
+
+	void CircleCauchyFunction_Delete(const CircleCauchyFunction& self) {
+		delete &self;
+	}
+
+	SpiralCauchyFunction* SpiralCauchyFunction_New() {
+		return new SpiralCauchyFunction();
+	}
+
+	void SpiralCauchyFunction_Delete(const SpiralCauchyFunction& self) {
+		delete &self;
 	}
 }
 
